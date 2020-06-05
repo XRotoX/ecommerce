939ee61a72c3abaad3a4d5c012fb1342fb6f5858
Rotoo <mohammedghaouti@gmail.com>
2020-06-05 19:02:50 +0100

    mohammedghaouti@gmail.com

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, "statics", "our_static")
+    os.path.join(BASE_DIR, "statics", "our_static"),
     os.path.join(BASE_DIR, "media", "products")
 )
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
